e62d1d173339a76a09a45e384a11e2a0bcc478bd
XixianLiang <51265902107@stu.ecnu.edu.cn>
2025-04-17 22:03:39 +0800

    update element info filter

--- hdc/window_manager.py
+++ hdc/window_manager.py
@@ -8,7 +8,7 @@
 from .system import _execute_command
 from .Component import ComponentNode
 import re
-    
+
 
 async def click(center) -> bool:
     """
@@ -71,7 +71,7 @@ async def screen_state() -> str:
         match = re.search(pattern, data)
 
         return match.group(1) if match else None
-    
+
 async def wakeup():
     """
     wake up the phone
@@ -87,7 +87,7 @@ async def dump_hierarchy():
     _tmp_path = f"/data/local/tmp/hierarchy_tmp.json"
     await _execute_command(f"hdc shell uitest dumpLayout -p {_tmp_path}")
     await _execute_command(f"hdc file recv {_tmp_path} ./tmp.json")
-    
+
 async def get_uilayout() -> str:
     """
     Retrieves information about clickable elements in the current UI.
@@ -121,27 +121,36 @@ async def get_uilayout() -> str:
         text = node_info["text"].strip()
         desc = node_info["description"].strip()
         bounds = node_info["bounds"].strip()
-        clickable = node_info["clickable"].strip()
-        id = node_info["id"].strip()
-
-        if clickable == "true" or text or desc:
-            if clickable == "true":
-                element_info = "Clickable element:"
+        clickable = node_info["clickable"].strip() == "true"
+        editable = node_info["type"] in ["Text", "TextInput", "SearchField"] and clickable
+        key = node_info["key"].strip()
+        
+        if any([text, desc]):
+            element_infos = []
+            element_type = "Element Type: "
+            if not any([clickable, editable, key]):
+                element_type += "Plaintext"
             else:
-                element_info = "Text element"
-            center = calculate_center(bounds)
-            if id:
-                element_info += f"\n Id: {id}"
-            if text:
-                element_info += f"\n  Text: {text}"
-            if desc:
-                element_info += f"\n  Description: {desc}"
-            if center:
-                element_info += f"\n  Center: ({center[0]}, {center[1]})"
-            element_info += f"\n  Bounds: {bounds}"
-            clickable_elements.append(element_info)
+                element_type += "Clickable " if clickable or key else ""
+                element_type += "Editable " if editable or key else ""
+            element_infos.append(element_type)
 
+            center = calculate_center(bounds) 
+            if key:
+                element_infos.append(f"Key: {key}")
+            if text:
+                element_infos.append(f"Text: {text}")
+            if desc:
+                element_infos.append(f"Description: {desc}")
+            if center:
+                element_infos.append(f"Center: ({center[0]}, {center[1]})")
+            element_infos.append(f"Bounds: {bounds}")
+
+            clickable_elements.append("\n    ".join(element_infos))
         for child in root["children"]:
+            # skip the system status bar
+            if child["attributes"].get("bundleName", "") == "com.ohos.sceneboard":
+                continue
             traverseTree(child)
 
     import json
